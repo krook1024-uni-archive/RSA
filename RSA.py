c76ab8cf0daa9d550da098cb52d95b82ce5a370a
Molnár Antal Albert <krook1024@gmail.com>
2020-05-25 12:47:13 +0200

    docs

--- RSA.py
+++ RSA.py
@@ -1,5 +1,5 @@
 from math import gcd
-from random import randint, sample
+from random import randint
 
 
 class RSA:
@@ -31,10 +31,18 @@ class RSA:
         return (self.p - 1) * (self.q - 1)
 
     def find_primes(self):
+        """
+        Beállít egy-egy prímszámot p és q értékének a példányon belül.
+        :return:
+        """
         self.p = RSA.random_prime()
         self.q = RSA.random_prime()
 
     def find_keys(self):
+        """
+        Megkeresi a privát és nyílt kulcsokat.
+        :return:
+        """
         for e in range(3, self.phin, 2):
             if gcd(e, self.phin) == 1 and 1 < e < self.phin:
                 self.e = e
@@ -46,6 +54,11 @@ class RSA:
             self.d += self.phin
 
     def encrypt(self, string):
+        """
+        Titkosítja a stringet a példányváltozók segítségével.
+        :param string: a titkos üzenet
+        :return:
+        """
         def str_to_int(m):
             return int("".join([str(ord(x) - 64) for x in m]))
 
@@ -57,6 +70,11 @@ class RSA:
             raise ValueError("String too long for encryption (m >= n)")
 
     def decrypt(self, ciphertext):
+        """
+        Visszafejti a ciphertextet a példányváltozók segítségével
+        :param ciphertext: a titkosított üzenet
+        :return: a tiszta üzenet
+        """
         def int_to_str(c):
             cstr = str(c)
             return "".join([chr(int(cstr[i:i + 2]) + 64) for i in range(0, len(cstr), 2)])
@@ -66,14 +84,24 @@ class RSA:
 
     @staticmethod
     def random_prime():
+        """
+        Véletlenszerűen választ egy prímet.
+        :return: egy prímszám
+        """
         while True:
             p = randint(10000000, 100000000)
 
-            if RSA.miller_rabin_test(p, 3):
+            if RSA.miller_rabin_test(p):
                 return p
 
     @staticmethod
     def gcde(a, b):
+        """
+        A kiterjesztett euklideszi algoritmus.
+        :param a: az egyik szám
+        :param b:  a másik szám
+        :return: gcd(a, b), x, y
+        """
         x0, x1, y0, y1 = 0, 1, 1, 0
         while a != 0:
             (q, a), b = divmod(b, a), a
@@ -82,36 +110,47 @@ class RSA:
         return b, x0, y0
 
     @staticmethod
-    def miller_rabin_test(n, k=7):
-        """use Rabin-Miller algorithm to return True (n is probably prime)
-           or False (n is definitely composite)"""
-        if n < 6:  # assuming n >= 0 in all cases... shortcut small cases here
+    def miller_rabin_test(n, k=3):
+        """
+        Megvizsgál egy számot összetettsége szempontjából.
+
+        :param n a szám
+        :param k a tesztek száma
+        :return True, ha lehetséges, hogy n prím, False ha n biztosan összetett
+        """
+        if n < 6:  # 0-tól nagyobb és 6-nál kisebb esetekre egy shortcut
             return [False, False, True, True, False, True][n]
-        elif n & 1 == 0:  # should be faster than n % 2
+        elif n % 2 == 0: # n páros
             return False
         else:
             s, d = 0, n - 1
+
+            # n - 1 = 2 ^ s * d
             while d & 1 == 0:
-                s, d = s + 1, d >> 1
-            # Use random.randint(2, n-2) for very large numbers
-            for a in sample(range(2, min(n - 2, float("inf"))), min(n - 4, k)):
+                s += 1
+                d >>= 1
+
+            # k-szor választunk tetszőleges a-t, a < n
+            for a in [randint(2, n-2) for _ in range(k)]:
                 x = pow(a, d, n)
                 if x != 1 and x + 1 != n:
-                    for r in range(1, s):
+                    for r in range(s): # 0 -> s-1
                         x = pow(x, 2, n)
+
                         if x == 1:
-                            return False  # composite for sure
+                            return False # biztosan összetett
                         elif x == n - 1:
-                            a = 0  # so we know loop didn't continue to end
-                            break  # could be strong liar, try another a
-                    if a:
-                        return False  # composite if we reached end of this loop
-            return True  # probably prime if reached end of outer loop
+                            a = 0
+                            break
+                    if a != 0:
+                        return False # ha idáig eljut, összetett
+            return True
 
 
 def main():
     for _ in range(30):
         rsa = RSA()
+        print(rsa)
 
         clear = 'teszt'
         ciphertext = rsa.encrypt(clear)
